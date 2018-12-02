a7cb4694bc3c9b31b77889c737be82ebb8367968
zhansan <zhangsan@itcast.cn>
2018-12-02 09:59:01 +0800

    222222222

--- git-test/src/Test.java
+++ git-test/src/Test.java
@@ -9,4 +9,10 @@ public class Test {
         System.out.println("111111111");
         System.out.println("111111111");
     }
+    public void test(){
+        int i =0;
+        i+=10;
+
+
+    }
 }
